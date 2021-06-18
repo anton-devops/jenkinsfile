5619e5b5aff193ef8fc00d7e95de4581fd171f79
Anton Akhramovich <akhramovich.anton@gmail.com>
2021-06-18 23:18:53 +0300

    rm jenkins-agent-wp-plugin.sh

--- jenkins-agent-wp-plugin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo 46039ea47beb8df7eba62fe1797b734530000e68b59659fcdd9d69aa0c99bdfa > secret-file
-java -jar agent.jar -jnlpUrl http://ip172-18-0-124-c36908vnjsv000e5hhk0-8080.direct.labs.play-with-docker.com/computer/test/jenkins-agent.jnlp -secret @secret-file -workDir "/home/ubuntu/"
-
